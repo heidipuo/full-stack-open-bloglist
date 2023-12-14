f7fc85e8cb8cec72ea168a3cf9e8219fc7009eac
Heidi <puotiniemi.heidi@gmail.com>
2023-12-14 11:42:08 +0200

    configurated port settings

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-app.listen(config.PORT, () => {
+const PORT = config.PORT || 3003
+
+app.listen(PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
